329c0dc3b4cc95ef9190ede0bcc94ae0aab4dbc7
Nifemi Adewusi <nifemiadewusi@gmail.com>
2024-03-21 16:08:19 +0100

    Update Currency Format

--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -1,17 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
-const productionUrl = ' https://strapi-store-server.onrender.com/api';
+const productionUrl = " https://strapi-store-server.onrender.com/api";
 
 export const customFetch = axios.create({
   baseURL: productionUrl,
 });
 
 export const formatPrice = (price) => {
-  const dollarsAmount = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
+  const nairaAmount = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "NGN",
   }).format((price / 100).toFixed(2));
-  return dollarsAmount;
+  return nairaAmount;
 };
 
 export const generateAmountOptions = (number) => {
