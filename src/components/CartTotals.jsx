4e407f664b0bfc05f140b99bd87550a9651d34ab
Nifemi Adewusi <nifemiadewusi@gmail.com>
2026-02-09 17:08:53 +0100

    Updates

--- src/components/CartTotals.jsx
+++ src/components/CartTotals.jsx
@@ -1,33 +1,33 @@
-import { useSelector } from 'react-redux';
-import { formatPrice } from '../utils';
+import { useSelector } from "react-redux";
+import { formatPrice } from "../utils";
 
 const CartTotals = () => {
   const { cartTotal, shipping, tax, orderTotal } = useSelector(
-    (state) => state.cartState
+    (state) => state.cartState,
   );
 
   return (
-    <div className='card bg-base-200'>
-      <div className='card-body'>
+    <div className="card bg-base-200">
+      <div className="card-body">
         {/* SUBTOTAL */}
-        <p className='flex justify-between text-xs border-b border-base-300 pb-2'>
+        <p className="flex justify-between text-sm border-b border-base-300 pb-2">
           <span>Subtotal</span>
-          <span className='font-medium'>{formatPrice(cartTotal)}</span>
+          <span className="font-medium">{formatPrice(cartTotal)}</span>
         </p>
         {/* SHIPPING */}
-        <p className='flex justify-between text-xs border-b border-base-300 pb-2'>
+        <p className="flex justify-between text-sm border-b border-base-300 pb-2">
           <span>Shipping</span>
-          <span className='font-medium'>{formatPrice(shipping)}</span>
+          <span className="font-medium">{formatPrice(shipping)}</span>
         </p>
         {/* Tax */}
-        <p className='flex justify-between text-xs border-b border-base-300 pb-2'>
+        <p className="flex justify-between text-sm border-b border-base-300 pb-2">
           <span>Tax</span>
-          <span className='font-medium'>{formatPrice(tax)}</span>
+          <span className="font-medium">{formatPrice(tax)}</span>
         </p>
         {/* Order Total */}
-        <p className='flex justify-between text-sm mt-4 pb-2'>
+        <p className="flex justify-between text-xl font-bold mt-4 pb-2">
           <span>Order Total</span>
-          <span className='font-medium'>{formatPrice(orderTotal)}</span>
+          <span className="font-medium">{formatPrice(orderTotal)}</span>
         </p>
       </div>
     </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,37 +1,48 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import Typewriter from "./Typewriter";
 
-import hero1 from '../assets/hero1.webp';
-import hero2 from '../assets/hero2.webp';
-import hero3 from '../assets/hero3.webp';
-import hero4 from '../assets/hero4.webp';
+import hero1 from "../assets/hero1.webp";
+import hero2 from "../assets/hero2.webp";
+import hero3 from "../assets/hero3.webp";
+import hero4 from "../assets/hero4.webp";
 
 const carouselImages = [hero1, hero2, hero3, hero4];
 
 const Hero = () => {
   return (
-    <div className='grid lg:grid-cols-2 gap-24 items-center'>
+    <div className="grid lg:grid-cols-2 gap-24 items-center">
       <div>
-        <h1 className='max-w-2xl text-4xl font-bold tracking-tight sm:text-6xl'>
-          We are changing the way people shop
+        <h1 className="max-w-2xl text-4xl font-bold tracking-tight sm:text-6xl">
+          Experience{" "}
+          <span className="text-primary">
+            <Typewriter
+              text={["Comfort", "Style", "Quality"]}
+              delay={2000}
+              infinite
+              typeSpeed={300}
+              deleteSpeed={150}
+            />
+          </span>
+          <br /> Redefined.
         </h1>
-        <p className='mt-8 max-w-xl text-lg leading-8'>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore
-          repellat explicabo enim soluta temporibus asperiores aut obcaecati
-          perferendis porro nobis.
+        <p className="mt-8 max-w-xl text-lg leading-8">
+          Discover a curated collection of premium furniture and decor that
+          transforms your house into a home. Elevate your living space with
+          Comfy.
         </p>
-        <div className='mt-10'>
-          <Link to='/products' className='btn btn-primary'>
-            Our Products
+        <div className="mt-10">
+          <Link to="/products" className="btn btn-primary">
+            Shop Now
           </Link>
         </div>
       </div>
-      <div className='hidden h-[28rem] lg:carousel carousel-center p-4 space-x-4 bg-neutral rounded-box '>
+      <div className="hidden h-[28rem] lg:carousel carousel-center p-4 space-x-4 bg-neutral rounded-box ">
         {carouselImages.map((image) => {
           return (
-            <div key={image} className='carousel-item'>
+            <div key={image} className="carousel-item">
               <img
                 src={image}
-                className='rounded-box h-full w-80 object-cover'
+                className="rounded-box h-full w-80 object-cover"
               />
             </div>
           );
