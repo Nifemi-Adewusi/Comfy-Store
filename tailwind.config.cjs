2c964fad9b267e59cc504caf0d55734a213a681b
Nifemi Adewusi <nifemiadewusi@gmail.com>
2026-02-27 10:53:52 +0100

    Add the Roboto font family by extending the font family of tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: "Roboto",
+      },
+    },
   },
-  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
-    themes: ['winter', 'dracula'],
+    themes: ["winter", "dracula"],
   },
 };
